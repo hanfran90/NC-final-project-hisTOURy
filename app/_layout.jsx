78f8bf06e0ae7df7824e58865f76e0c9a3582bfb
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-18 15:32:52 +0000

    adds tailwind

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,13 +3,14 @@ import { QueryClient } from "@tanstack/react-query";
 import { Slot } from "expo-router";
 import React from "react";
 import { View } from "react-native";
+import "../global.css";
 
 const queryClient = new QueryClient();
 
 function _layout() {
   useReactQueryDevTools(queryClient);
   return (
-    <View>
+    <View className="p-4">
       <Slot />
     </View>
   );
