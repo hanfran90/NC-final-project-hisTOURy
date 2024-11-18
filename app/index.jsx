f9a408572af74ddbe4d78683e56f0442780c538f
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-18 15:46:48 +0000

    cleanup

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Text } from "react-native";
-import { supabase } from "../utils/supabaseClient";
 
 function index() {
-  useEffect(() => {
-    supabase
-      .from("todos")
-      .select()
-      .then((data) => {
-        console.log(data);
-      });
-  }, []);
-
   return <Text>Hello World</Text>;
 }
 
