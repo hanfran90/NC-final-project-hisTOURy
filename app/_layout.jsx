20c8cfedbdab60df7aee31846df60309c9bd1498
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-19 22:45:58 +0000

    working tabs

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,21 +1,20 @@
 import { useReactQueryDevTools } from "@dev-plugins/react-query/build/useReactQueryDevTools";
-import { QueryClient } from "@tanstack/react-query";
-import { Slot } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Stack } from "expo-router";
 import React from "react";
 import "../global.css";
-import { SafeAreaView } from "react-native-safe-area-context";
-import NavigationBar from "../components/NavigationBar";
 
 const queryClient = new QueryClient();
 
 function _layout() {
-	useReactQueryDevTools(queryClient);
-	return (
-		<SafeAreaView className="p-4">
-			<Slot />
-			<NavigationBar/>
-		</SafeAreaView>
-	);
+  useReactQueryDevTools(queryClient);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </QueryClientProvider>
+  );
 }
 
 export default _layout;
