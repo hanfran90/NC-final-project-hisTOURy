e2e87b0837ae920cb83652770e951dd82ffdf437
riona mccumskay <r.mccumskay@gmail.com>
2024-11-21 10:08:56 +0000

    spot changed to modal

--- app/(tabs)/explore/_layout.jsx
+++ app/(tabs)/explore/_layout.jsx
@@ -1,17 +1,24 @@
 import { Stack } from "expo-router";
+import { Screen } from "expo-router/build/views/Screen";
 import React from "react";
 
 export default function _layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen
+      {/* <Stack.Screen
         name="[spotId]"
         options={{
           title: "Spot",
         }}
-      />
+      /> */}
       <Stack.Screen name="add-spot" options={{ title: "Add Spot" }} />
+      <Screen
+          name="[spotId]"
+          options={{
+            presentation: "modal",
+          }}
+        />
     </Stack>
   );
 }
