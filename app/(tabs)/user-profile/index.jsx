dc3b96ff362f4cba28e8515d71fe83b715821979
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-21 16:53:25 +0000

    login temp fix

--- app/(tabs)/user-profile/index.jsx
+++ app/(tabs)/user-profile/index.jsx
@@ -1,20 +1,26 @@
-import { Link, Redirect } from "expo-router";
+import { Link, Redirect, useNavigation } from "expo-router";
 import { Button, StyleSheet, Text, View } from "react-native";
 
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../../../components/Auth/AuthContext";
 
 export default function Tab() {
   const { user, signOut } = useContext(AuthContext);
 
-  if (!user) return <Redirect href="/login" />;
+  const navigation = useNavigation();
 
-  return (
+  useEffect(() => {
+    if (!user) navigation.navigate("login");
+  }, []);
+
+  return user ? (
     <View style={styles.container}>
       <Text>User Profile</Text>
       <Link href="/user-profile/my-spots">My Spots</Link>
       <Button title="Logout" onPress={signOut} />
     </View>
+  ) : (
+    <Link href="/login">Login First</Link>
   );
 }
 
