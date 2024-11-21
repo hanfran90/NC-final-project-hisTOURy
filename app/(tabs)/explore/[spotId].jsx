7567825e973fe2c11b1c2205e8796c60097b96fe
vikkiChipset <vikkitorriya@gmail.com>
2024-11-21 15:08:55 +0000

    add SingleMarkerCard

--- app/(tabs)/explore/[spotId].jsx
+++ app/(tabs)/explore/[spotId].jsx
@@ -1,8 +1,41 @@
 import { useLocalSearchParams } from "expo-router";
-import { Text } from "react-native";
+import { Text, View, ActivityIndicator } from "react-native";
+import SingleMarkerCard from "../../../components/SingleMarkerCard";
+import useMarkerInfo from "../../../hooks/useMarkerInfo";
 
 export default function SpotDetails() {
   const { spotId } = useLocalSearchParams();
 
-  return <Text>Page: Spot Details - {spotId}</Text>;
+  const { data, isPending, error } = useMarkerInfo(spotId);
+
+  if (isPending) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View>
+        <Text>Error: {error.message || "Something went wrong."}</Text>
+      </View>
+    );
+  }
+
+  if (!data) {
+    return (
+      <View>
+        <Text>No data available.</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      {/* <Text>Page: Spot Details - {spotId}</Text> */}
+      <SingleMarkerCard markerData={data}/>
+    </View>
+  );
 }
