fad1a2fc50490319630aa5f9c1bb76e42a77a0da
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-22 14:04:37 +0000

    move fetching to tanstack query

--- app/planner.jsx
+++ app/planner.jsx
@@ -1,29 +1,19 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Button, FlatList, Text, View } from "react-native";
-import { supabase } from "../utils/supabaseClient";
+import React, { useContext } from "react";
+import { FlatList, Text, View } from "react-native";
 import { AuthContext } from "../components/Auth/AuthContext";
+import useUserPlanner from "../hooks/useUserPlanner";
 
 export default function planner() {
   const { user } = useContext(AuthContext);
-  l;
-  const [profile, setProfile] = useState({});
+  const { data: planner } = useUserPlanner(user.id);
 
-  useEffect(() => {
-    if (user)
-      supabase
-        .from("profiles")
-        .select("user_id, planner:users_markers!inner(markers(*))")
-        .eq("user_id", user.id)
-        .then((res) => {
-          setProfile(res.data[0]);
-        });
-  }, [user]);
+  console.log({ user, planner });
 
   return (
     <View>
       <Text>planner Page</Text>
       <FlatList
-        data={profile.planner}
+        data={planner}
         renderItem={({ item: { markers }, index }) => (
           <Text>
             {index}: {JSON.stringify(markers)}
