4cd5c16b5a2f95097c925ddb7c40828dda5f73f6
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-24 15:29:56 +0000

    adds useUserAddToPlanner

---
+++ hooks/useUserAddToPlanner.js
@@ -0,0 +1,31 @@
+import { useMutation } from "@tanstack/react-query";
+import { supabase } from "../utils/supabaseClient";
+import useUserPlanner from "./useUserPlanner";
+
+export default function useUserAddToPlanner(marker_id) {
+  const { data: planners, refetch } = useUserPlanner();
+
+  const defaultPlannerId = planners[0]?.planner_id;
+  const canAddToPlanner =
+    defaultPlannerId &&
+    marker_id &&
+    planners[0].items.every(
+      ({ marker: { marker_id: mid } }) => mid !== marker_id
+    );
+
+  const mutation = useMutation({
+    mutationKey: ["add-to-planner"],
+    mutationFn: () =>
+      supabase
+        .from("planners_markers")
+        .insert({
+          planner_id: defaultPlannerId,
+          marker_id,
+        })
+        .select(),
+    onSuccess: () => refetch(),
+    enable: canAddToPlanner,
+  });
+
+  return { canAddToPlanner, ...mutation };
+}
