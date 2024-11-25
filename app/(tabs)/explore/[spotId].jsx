ca3a0e8922c5e5ec34134db9ec16a16fc049585c
vikkiChipset <vikkitorriya@gmail.com>
2024-11-25 09:14:10 +0000

    add design login page

--- app/(tabs)/explore/[spotId].jsx
+++ app/(tabs)/explore/[spotId].jsx
@@ -3,10 +3,14 @@ import { Text, View, ActivityIndicator, Button } from "react-native";
 import SingleMarkerCard from "../../../components/SingleMarkerCard";
 import useMarkerInfo from "../../../hooks/useMarkerInfo";
 import { supabase } from "../../../utils/supabaseClient";
+import { TouchableOpacity } from "react-native";
+import { useContext } from "react";
+import { AuthContext } from "../../../components/Auth/AuthContext";
 
 export default function SpotDetails() {
   const { spotId } = useLocalSearchParams();
-
+  // const { user } = useContext(AuthContext);
+  // console.log(user.id);
   const { data, isPending, error } = useMarkerInfo(spotId);
 
   if (isPending) {
@@ -47,10 +51,17 @@ export default function SpotDetails() {
       });
   }
   return (
-    <View>
-      {/* <Text>Page: Spot Details - {spotId}</Text> */}
+    <View className="p-4">
       <SingleMarkerCard markerData={data} />
-      <Button title="Add to planner" onPress={handleOnPress} />
+      <TouchableOpacity
+        className="bg-blue-500 py-2 px-4 rounded-full mt-4"
+        onPress={handleOnPress}
+      >
+        <Text className="text-white text-center font-semibold">
+          Add to planner
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
--- components/SingleMarkerCard.jsx
+++ components/SingleMarkerCard.jsx
@@ -1,19 +1,35 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, View, Image } from "react-native";
+
 
 export default function SingleMarkerCard({ markerData }) {
   return (
     <>
-    <View className="m-4 mb-0 last-child:mb-4 p-4 min-h-[100] flex justify-center content-center rounded-xl bg-white">
-      {markerData.title && (
-        <Text className="text-center text-xl font-bold">
-          {markerData.title}
+      <View className="m-4 p-4 rounded-xl bg-white shadow-sm space-y-4">
+        {markerData.title && (
+          <Text className="text-center text-3xl font-extrabold text-gray-800">
+            {markerData.title}
+          </Text>
+        )}
+      </View>
+      <View>
+        <View className="m-4 mb-0 last-child:mb-4 p-4 min-h-[100] flex justify-center content-center rounded-xl bg-white">
+          <Text>
+            {markerData.photo} There will be nice photo from Hannah and Georgia
+          </Text>
+          <Image
+            source={{
+              uri: "https://www.dfc.co.uk/wp-content/uploads/2018/09/MTC-Oxford-House_taken-by-NS_0869.png",
+            }}
+            className="m-4 h-40 rounded-xl"
+          />
+        </View>
+        <Text className="text-gray-600 text-base font-medium mb-2">
+          {markerData.address} Address: should be here
         </Text>
-      )}
-    </View>
-    <View>
-          <Text>{markerData.address} Address: should be here</Text>
-          <Text>{markerData.description} Description: should be here</Text>
-    </View>
+        <Text className="text-gray-700 text-sm">
+          {markerData.description} Description: should be here
+        </Text>
+      </View>
     </>
   );
 }
