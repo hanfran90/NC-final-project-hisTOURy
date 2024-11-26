3ed36209985100487a18654d8f203dc3cfc2eb20
Hannah Reed <hannahfrancesreed@gmail.com>
2024-11-26 20:41:07 +0000

    Option Search Bar test for map

--- app/(tabs)/explore/index.jsx
+++ app/(tabs)/explore/index.jsx
@@ -5,10 +5,14 @@ import FloatingAction from "../../../components/FloatingAction";
 import InteractiveMap from "../../../components/InteractiveMap";
 import MapLayerPlanner from "../../../components/MapLayerPlanner";
 import useCurrentLocation from "../../../hooks/useCurrentLocation";
+import SearchBar from "../../../components/SearchBar";
+import DropdownSearch from "../../../components/DropDownSearch";
+import { useState } from "react";
 
 export default function Explore() {
   const { route } = useLocalSearchParams();
   const { location, isPending, error } = useCurrentLocation();
+  const [selectedOption, setSelectedOption] = useState("");
 
   if (isPending) return <Text>Pending...</Text>;
   if (error) return <Text>{JSON.stringify(error)}</Text>;
@@ -16,8 +20,26 @@ export default function Explore() {
 
   console.log({ route });
 
+  const options = [
+    "Map",
+    "Museum",
+    "Church",
+    "Gallery",
+    "Statue",
+    "Shorts",
+    "David",
+  ];
+  const handleOptionSelected = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
     <>
+      {/* <SearchBar /> */}
+      <DropdownSearch
+        options={options}
+        onOptionSelected={handleOptionSelected}
+      />
       <View style={{ height: "100%" }}>
         <InteractiveMap
           coords={[location.longitude, location.latitude]}
