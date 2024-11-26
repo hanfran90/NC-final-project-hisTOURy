c4ec78cf62c47766a61722916f73a7c55b55179b
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-26 19:23:34 +0000

    add valid sql

--- supabase/migrations/20241126121929_add_func_update_planner_sequence.sql
+++ supabase/migrations/20241126121929_add_func_update_planner_sequence.sql
@@ -1,5 +1,5 @@
 DROP FUNCTION IF EXISTS public.update_planner_sequence CASCADE;
-CREATE FUNCTION public.update_planner_sequence (body jsonb) TABLE (planner_id INT, marker_id INT, sequence INT) AS $$
+CREATE FUNCTION public.update_planner_sequence (body jsonb) RETURNS VOID AS $$
 DECLARE
     plannerId int;
     marker jsonb;
@@ -9,7 +9,7 @@ BEGIN
     plannerId := (body->'planner_id')::int;
 
     FOR marker IN
-        SELECT * FROM jsonb_array_elements(body->'markers')
+        SELECT * FROM jsonb_array_elements(body->'items')
     LOOP
         markerId := (marker->'marker_id')::int;
         seq := (marker->'sequence')::int;
@@ -17,7 +17,6 @@ BEGIN
         UPDATE public.planners_markers
             SET "sequence" = seq
             WHERE planner_id = plannerId AND marker_id = markerId;
-            RETURNING planner_id, marker_id, sequence;
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
